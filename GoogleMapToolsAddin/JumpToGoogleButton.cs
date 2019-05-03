d7868bf7355f6baf1f928386ab426fe60650a5b7
mwashbur <35734485+mwashbur@users.noreply.github.com>
2019-05-03 17:46:24 -0500

    Add files via upload

---
+++ GoogleMapToolsAddin/JumpToGoogleButton.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+
+namespace GoogleMapToolsAddin
+{
+    internal class JumpToGoogleButton : Button
+    {
+        protected override void OnClick()
+        {
+            QueuedTask.Run(() =>
+            {
+                //get active MapView
+                MapView theView = MapView.Active;
+
+                //get current spatial reference for debugger
+                SpatialReference currentSR = theView.Map.SpatialReference;
+
+                //get map center and project to GCS
+                MapPoint mapCenter = theView.Extent.Center;
+                MapPoint theCoords = (MapPoint)GeometryEngine.Instance.Project(mapCenter, SpatialReferences.WGS84);
+
+                //get map scale
+                double theScale = theView.Camera.Scale;
+
+                //calculate google map zoom level equivalent and round to nearest quarter
+                double theZoom = Math.Round(
+                    4 * (Math.Log(591657550.500000 / (theScale / 2)) / Math.Log(2)), MidpointRounding.ToEven) / 4;
+
+                //message box for debugging
+                //ArcGIS.Desktop.Framework.Dialogs.MessageBox.Show(string.Format(
+                //    "Center X: {0} Center Y: {1} Map Scale: {2} Google Zoom Level: {3}", 
+                //    theCoords.X, theCoords.Y, theScale, theZoom), "Results");
+
+                //open in browser
+                string googleURL = "https://www.google.com/maps/@";
+                string url = string.Format(googleURL+"{0},{1},{2}z", 
+                    theCoords.Y, theCoords.X, theZoom);
+                Process.Start(url);
+
+            });
+        }
+    }
+}
---
+++ GoogleMapToolsAddin/JumpToStreetviewTool.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
+using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+
+namespace GoogleMapToolsAddin
+{
+    internal class JumpToStreetviewTool : MapTool
+    {
+        protected override void OnToolMouseDown(MapViewMouseButtonEventArgs e)
+        {
+            //Handle the event args to get the call to the corresponding async method
+            if (e.ChangedButton == System.Windows.Input.MouseButton.Left)
+                e.Handled = true; 
+        }
+
+        // Get the point in client coordinates relative to the top-left corner of the view
+        protected override Task HandleMouseDownAsync(MapViewMouseButtonEventArgs e)
+        {
+            return QueuedTask.Run(() =>
+            {
+                // Convert the clicked point in client coordinates to the corresponding map coordinates.
+                MapPoint thePoint = MapView.Active.ClientToMap(e.ClientPoint);
+
+                // Message box for debugging
+                //ArcGIS.Desktop.Framework.Dialogs.MessageBox.Show(string.Format("X: {0} Y: {1}",
+                //   thePoint.X, thePoint.Y), "Map Coordinates");
+
+                // Convert to GCS
+                MapPoint theCoords = (MapPoint)GeometryEngine.Instance.Project(thePoint, SpatialReferences.WGS84);
+
+                // Open a web browser
+                string googleURL = "https://www.google.com/maps/@?api=1&map_action=pano&viewpoint=";
+                string url = string.Format(googleURL + "{0},{1}", theCoords.Y, theCoords.X);
+                Process.Start(url);
+
+                // Reset default mouse behavior
+                FrameworkApplication.SetCurrentToolAsync("esri_mapping_exploreTool"); 
+
+            });
+        }
+
+    }
+}
